399a11564232953e01f99131fc242df79212d095
Emeline <emeline.terre@gmail.com>
2021-05-17 12:20:43 +0200

    debut ajout de listen a BDD front/back

--- reactapp/src/views/ScreenListen.js
+++ reactapp/src/views/ScreenListen.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import '../App.css';
 import { Slider,Input,Button,Form,Row,Col} from 'antd'
 import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
@@ -16,6 +16,14 @@ function ScreenListen(props) {
    var handleChange = (value) => {
         setMoodValue(value);
       };
+     // Enregistrement du Listen Collab
+    var saveListenCollab = (valueMood,value1,value2,value3,value4,value5) => {
+        await fetch('/save-listen', {
+        method: 'PUT',
+        headers: {'Content-Type':'application/x-www-form-urlencoded'},
+        body: `_id:${props.user._id}&mood=${valueMood}&reponse1=${value1}&reponse2=${value2}&reponse3=${value3}&reponse4=${value4}&reponse5=${value5}`
+        });
+    };
 
   return (
       
@@ -91,7 +99,7 @@ function ScreenListen(props) {
                     <Form.Item >
                         <Button htmlType="submit" className='input-button-listen'
                         style={{backgroundColor:'#3d84b8',color:'white'}}
-                        onClick={()=> console.log('test',moodValue, responseOne, responseTwo, responseThree, responseFour,responseFive) }>
+                        onClick={()=> saveListenCollab(moodValue,responseOne,responseTwo,responseThree,responseFour,responseFive) }>
                         Valider
                         </Button>
                     </Form.Item>
--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,20 @@ router.get('/new-campaign', function(req, res, next) {
   res.json ({response: 'Nouvelle campagne lancée'})
  });
 
+ router.put('/save-listen', async function(req, res, next) {
+    let _id = req.body._id 
+
+    var saveNewListen = await ListenModel.updateOne({collab : _id, isActive : true},
+      
+      )
+    
+  
+  // Update ancienne campagne, tous les listen dont managerId et isActive =true ==> isActive = false
+  // Find template default
+  // Création listens avec managerId from team et collabId from team
+  res.json ({response: 'Nouvelle campagne lancée'})
+ });
+
 
 
 module.exports = router;
