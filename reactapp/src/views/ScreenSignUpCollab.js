2d0d984097a058acc9522a24533a8a06ad591a81
Soraya Cantos <sorayacantos@gmail.com>
2021-05-17 10:56:05 +0200

    écrans sign-up terminés

--- reactapp/src/views/ScreenSignUpCollab.js
+++ reactapp/src/views/ScreenSignUpCollab.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {Col, Input, Row, Alert, Space, Button} from 'antd';
+import {Link} from 'react-router-dom';
+
 function ScreenSignUpCollab() {
 
     return (
         <div className="background">
           <Row justify="center" align="middle" style={{height:'20%'}}>
           <Col span={16}>
-          <Alert
-      message="Bienvenu ! Veuillez remplir les informations ci-dessous pour vous inscrire."
+          <Alert style={{borderRadius: '5px'}}
+      message="Bienvenu ! Vous avez été invité par votre manager, veuillez remplir les informations ci-dessous pour vous inscrire."
       type="warning"
       action={
         <Space>
-          <Button size="small" type="ghost">
+          <Link to={'/'}><Button size="small" type="ghost">
             Je suis déjà inscrit
-          </Button>
+          </Button></Link>
         </Space>
       }
       closable
@@ -22,31 +24,31 @@ function ScreenSignUpCollab() {
           </Col>
           </Row>
           <Row justify="center" align="middle">
-            <Col span={16} align="middle" style={{fontSize: '2rem', display:'flex', justifyContent:'flex-start'}}>
+            <Col span={16} align="middle" className="sign-up-title">
             Mes informations personnelles :
             </Col>
             </Row>
             <Row justify="center" align="top">
             <Col span={16}>
-          <div style={{ display: 'flex', justifyContent: 'space-between'}}> 
-          <Input style={{borderRadius: '5px', width:'49%', margin:'2%'}} placeholder="Nom" />
-          <Input style={{borderRadius: '5px', width:'49%', margin:'2%'}} placeholder="Prénom" />
+          <div className="sign-up-input-group"> 
+          <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Nom" />
+          <Input  className="sign-up-inputs sign-up-top-input-width" placeholder="Prénom" />
           </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between'}}> 
-          <Input style={{borderRadius: '5px', width:'49%', margin:'2%'}} placeholder="Mot de passe" />
-          <Input style={{borderRadius: '5px', width:'49%', margin:'2%'}} placeholder="Confirmation du mot de passe" />
-          </div>
-          <div>
-          <Input style={{borderRadius: '5px', width:'98%', margin:'2%'}} placeholder="Entreprise" />
+          <div className="sign-up-input-group"> 
+          <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Mot de passe" />
+          <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Confirmation du mot de passe" />
           </div>
           <div>
-          <Input style={{borderRadius: '5px', width:'98%', margin:'2%'}} placeholder="Intitulé du poste" />
+          <Input className="sign-up-inputs sign-up-bottom-input-width" placeholder="Entreprise" />
+          </div>
+          <div>
+          <Input className="sign-up-inputs sign-up-bottom-input-width" placeholder="Intitulé du poste" />
           </div>
           </Col>
           </Row>
           <Row justify="center" align="center">
-            <Col span={16} style={{display:'flex', justifyContent:'flex-end'}}>
-          <Button style={{borderRadius: '5px', marginTop: '30px'}}>S'inscrire</Button>
+            <Col span={16} className="sign-up-button-div">
+          <Button className="sign-up-button">S'inscrire</Button>
           </Col>
           </Row>
         </div>
--- reactapp/src/views/ScreenSignUpManager.js
+++ reactapp/src/views/ScreenSignUpManager.js
@@ -1,16 +1,59 @@
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
-import { useParams } from "react-router-dom";
-import { Card, Icon, Modal, Button} from 'antd';
-import Nav from './Nav'
+import {Col, Input, Row, Alert, Space, Button} from 'antd';
+import {Link} from 'react-router-dom';
 
 function ScreenSignUpManager(props) {
 
     return (
-        <div className="background">
-        <Nav/>
-
-  </div>
+      <div className="background">
+      <Row justify="center" align="middle" style={{height:'20%'}}>
+      <Col span={16}>
+      <Alert style={{borderRadius: '5px'}}
+  message="Bienvenu ! Veuillez remplir les informations ci-dessous pour vous inscrire."
+  type="warning"
+  action={
+    <Space>
+       <Link to={'/'}>
+      <Button size="small" type="ghost">
+        Je suis déjà inscrit
+      </Button>
+      </Link>
+    </Space>
+  }
+  closable
+/>
+      </Col>
+      </Row>
+      <Row justify="center" align="middle">
+        <Col span={16} align="middle" className="sign-up-title">
+        Mes informations personnelles :
+        </Col>
+        </Row>
+        <Row justify="center" align="top">
+        <Col span={16}>
+      <div className="sign-up-input-group"> 
+      <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Nom" />
+      <Input  className="sign-up-inputs sign-up-top-input-width" placeholder="Prénom" />
+      </div>
+      <div className="sign-up-input-group"> 
+      <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Mot de passe" />
+      <Input className="sign-up-inputs sign-up-top-input-width" placeholder="Confirmation du mot de passe" />
+      </div>
+      <div>
+      <Input className="sign-up-inputs sign-up-bottom-input-width" placeholder="Entreprise" />
+      </div>
+      <div>
+      <Input className="sign-up-inputs sign-up-bottom-input-width" placeholder="Intitulé du poste" />
+      </div>
+      </Col>
+      </Row>
+      <Row justify="center" align="center">
+        <Col span={16} className="sign-up-button-div">
+      <Button className="sign-up-button">S'inscrire</Button>
+      </Col>
+      </Row>
+    </div>
     );
   }
   
