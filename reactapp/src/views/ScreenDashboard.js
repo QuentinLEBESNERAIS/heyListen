9d4493470a73c213cfba6e1875d0b4231278e80e
QuentinLB <quentinlebesnerais@Mini-de-Quentin.concorde-pereire.loc>
2021-05-21 14:53:56 +0200

    avant push

--- reactapp/src/views/ScreenDashboard.js
+++ reactapp/src/views/ScreenDashboard.js
@@ -36,7 +36,7 @@ useEffect(()=> {
   var getBddCollab = async () => {
   var rawResponse = await fetch(`/users/find-collab?manager=${props.userId._id}`);
   var collabs = await rawResponse.json();
-  //setTeam(collabs.collabs)
+  setTeam(collabs.collabs)
   setFilteredTeam(collabs.collabs)
   setListenfromBack(collabs.collabsListen)
   setFeedbackFromBack(collabs.collabFeedback)
@@ -63,7 +63,7 @@ var handleStatsRoute = async () =>{
     var getBddCollab = async () => {
     var rawResponse = await fetch(`/users/find-collab?manager=${props.userId._id}`);
     var collabs = await rawResponse.json();
-    //setTeam(collabs.collabs)
+    setTeam(collabs.collabs)
     setFilteredTeam(collabs.collabs)
     setListenfromBack(collabs.collabsListen)
     setFeedbackFromBack(collabs.collabFeedback)
