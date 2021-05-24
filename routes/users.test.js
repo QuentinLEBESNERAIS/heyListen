55e59055e06cd3f3acb7a98872dcdc3d96d72fe2
Soraya Cantos <sorayacantos@gmail.com>
2021-05-24 12:40:38 +0200

    ajout screen dashboard collab

--- routes/users.test.js
+++ routes/users.test.js
@@ -5,6 +5,6 @@ test("Sign-up Manager, plusieurs champs sont vides", async (done) => {
   await request(app).post('/users/sign-up-manager')
     .send({ lastName: 'Roger', firstName: 'Michel' })
     .expect(200)
-    .expect({response: 'renseigner tous les champs'});
+    .expect({response: 'Merci de renseigner tous les champs'});
   done();
 });
